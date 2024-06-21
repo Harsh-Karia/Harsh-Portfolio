14d2f69c118dadd745349b47f6e3acff506154fb
Harsh-Karia <64611524+Harsh-Karia@users.noreply.github.com>
2024-06-21 11:00:06 -0700

    Fixed demo links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,7 +26,7 @@ function Projects() {
               title="Enigma Coin"
               description="Enigma Coin is a blockchain-based cryptocurrency project developed from scratch using Python. The aim of this project was to explore the application of RSA encryption in blockchain technology. We created our own blockchain with RSA encryption and implemented various functionalities like mining, transactions, and coin buying. In addition, we developed a web platform to showcase the functioning of the Enigma Coin."
               ghLink="https://github.com/Mohnish2004/EnigmaCoin"
-              demoLink="https://github.com/Mohnish2004/EnigmaCoin"
+              // demoLink="https://github.com/Mohnish2004/EnigmaCoin"
             />
           </Col>
 
@@ -37,7 +37,7 @@ function Projects() {
               title="CerebralSecure"
               description="BCI Lock System employs a combination of jaw clenches, focus levels, and eye blinks by analyzing patterns derived from these inputs, the system can accurately identify an authorized user and grant or deny access accordingly."
               ghLink="https://github.com/Neurotech-Davis/CerebralSecure"
-              demoLink="https://github.com/Neurotech-Davis/CerebralSecure"
+              // demoLink="https://github.com/Neurotech-Davis/CerebralSecure"
             />
           </Col>
 
@@ -48,7 +48,7 @@ function Projects() {
               title="Quizzical"
               description="Quizzical is the perfect tool to use while studying. Simply upload your lecture notes, slides, or lecture recordings. Our efficient algorithm will scrape all of the relevant information from the uploaded files and generate custom flashcards for you to practice along with all the main points gathered from the file."
               ghLink="https://github.com/Harsh-Karia/file_to_flashcard"
-              demoLink="https://github.com/Harsh-Karia/file_to_flashcard"              
+              // demoLink="https://github.com/Harsh-Karia/file_to_flashcard"              
             />
           </Col>
 
@@ -59,7 +59,7 @@ function Projects() {
               title="FinGuard"
               description="FinGuard is a comprehensive platform tailored to guide recent college graduates and young professionals on their financial journey. Upon entering the platform, users can easily create an account, granting them access to our powerful financial tools. The centerpiece of FinGuard is our Loan Eligibility Predictor, which empowers users to gauge their eligibility for loans by inputting various factors like income, marital status, education, and loan terms."
               ghLink="https://github.com/SacHacks5-0/App"
-              demoLink="https://github.com/SacHacks5-0/App"
+              // demoLink="https://github.com/SacHacks5-0/App"
             />
           </Col>
 
