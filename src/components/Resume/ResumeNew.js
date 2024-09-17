63b941370b354b0a6b0bc5f14dce583a3badf799
Harsh-Karia <64611524+Harsh-Karia@users.noreply.github.com>
2024-09-17 11:58:55 -0700

    Added new resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Harsh_Karia_hnkaria@ucdavis.edu_MLH_Resume.pdf";
+import pdf from "../../Assets/../Assets/HarshKaria_Resume_hnkaria@ucdavis.edu_New.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
